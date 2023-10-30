0435757e891d17ff0708bd5a1d612b906320f830
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2023-10-30 18:11:44 +0530

    Bottom Navigation Bar

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -2,48 +2,40 @@ package com.example.tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.Button
-import android.widget.Spinner
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var FragmentOne: Fragment
     private lateinit var FragmentSecond: Fragment
+    private lateinit var FragmentThird: Fragment
+    private lateinit var bottomNavigationView: BottomNavigationView
 
-    private lateinit var btnFragmentOne: Button
-    private lateinit var btnFragmentSecond: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         FragmentOne = FragmentOne()
         FragmentSecond = FragmentSecond()
-        btnFragmentOne = findViewById(R.id.btnFirstFragment)
-        btnFragmentSecond = findViewById(R.id.btnSecondFragment)
+        FragmentThird = FragmentThird()
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        setCurrentFragment(FragmentOne)
 
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.miHome -> setCurrentFragment(FragmentOne)
+                R.id.miChat -> setCurrentFragment(FragmentSecond)
+                R.id.miProfile -> setCurrentFragment(FragmentThird)
+
+            }
+            true
+        }
+    }
+    private fun setCurrentFragment(fragment: Fragment)=
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.flFrameLayout, FragmentOne)
+            replace(R.id.flFragment, fragment)
             commit()
         }
-
-        btnFragmentOne.setOnClickListener{
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.flFrameLayout, FragmentOne)
-                commit()
-            }
-        }
-        btnFragmentSecond.setOnClickListener{
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.flFrameLayout, FragmentSecond)
-                commit()
-            }
-        }
-
-    }
 
 }
