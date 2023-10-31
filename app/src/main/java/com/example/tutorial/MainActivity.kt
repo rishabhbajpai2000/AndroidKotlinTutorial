cce8c14b1aa209cd645f02d4907d7f582258395f
Rishabh Bajpai <64163517+rishabhbajpai2000@users.noreply.github.com>
2023-10-31 16:21:52 +0530

    Slidable toolbar implementation video 26

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -2,32 +2,35 @@ package com.example.tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var FragmentOne: Fragment
-    private lateinit var FragmentSecond: Fragment
-    private lateinit var FragmentThird: Fragment
-    private lateinit var bottomNavigationView: BottomNavigationView
-    private lateinit var viewPager: ViewPager
-
-
+    private lateinit var toggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val images = listOf(
-            R.drawable.photo1,
-            R.drawable.photo2,
-            R.drawable.photo3
-        )
 
-        val adapter = ViewPagerAdapter(images)
-        viewPager.adapter = adapter
+        var drawerLayout:DrawerLayout = findViewById(R.id.drawerLayout)
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
